35e7aa94ee67d9ddc6c9d7fa7a5bc8e581b7267c
Poylar <doaleks8@gmail.com>
2023-05-26 17:28:25 +0800

    deploy

--- src/assets/js/SmoothScroll.js
+++ src/assets/js/SmoothScroll.js
@@ -19,17 +19,21 @@ archonLinks.forEach(t => {
   })
 })
 
-
 const counterElements = document.querySelectorAll('.features__value span');
 
 const animateNumber = (element, finalNumber) => {
+  // Проверяем, является ли число дробным
   const isDecimal = finalNumber % 1 !== 0;
-  if (!isNaN(finalNumber)) {
+
+  if (!isNaN(finalNumber) && typeof finalNumber === 'number') {
     gsap.to(element, {
       duration: 3,
+      // Если число дробное, используем parseFloat для преобразования строки в число с плавающей точкой
       innerHTML: isDecimal ? parseFloat(finalNumber) : finalNumber,
+      // Если число дробное, округляем его до двух знаков после запятой
       roundProps: isDecimal ? '' : 'innerHTML',
       onUpdate: () => {
+        // Если число дробное, округляем текущее значение до двух знаков после запятой
         if (isDecimal) {
           element.innerHTML = parseFloat(element.innerHTML).toFixed(2);
         } else {
@@ -43,7 +47,7 @@ const animateNumber = (element, finalNumber) => {
 gsap.registerPlugin(ScrollTrigger);
 
 counterElements.forEach((element) => {
-  const finalNumber = element.innerText;
+  const finalNumber = parseFloat(element.innerText);
   gsap.set(element, { innerHTML: 0 });
 
   ScrollTrigger.create({
