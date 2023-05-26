61fb81adb925ff6b038a44ad070460de31534649
Poylar <doaleks8@gmail.com>
2023-05-26 09:48:42 +0800

    deploy

--- src/assets/js/SmoothScroll.js
+++ src/assets/js/SmoothScroll.js
@@ -9,7 +9,7 @@ const smoother = ScrollSmoother.create({
 
 
 
-const archonLinks = document.querySelectorAll('js-scroll-to-section')
+const archonLinks = document.querySelectorAll('a[href^="#"]')
 archonLinks.forEach(t => {
   t.addEventListener('click', e => {
     e.preventDefault();
